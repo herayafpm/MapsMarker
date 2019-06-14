dd05354d0a52121f03355a64854ac279c214b675
Heraya <herayafpm@gmail.com>
2019-06-14 08:35:36 +0700

    maps marker v1

---
+++ app/src/main/java/com/heraya/mapsmarker/MainActivity.java
@@ -0,0 +1,75 @@
+package com.heraya.mapsmarker;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    EditText etSearch;
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(27.746974, 85.301582);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Kathmandu, Nepal"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
+    public void onMapSearch(View view) {
+
+        etSearch = findViewById(R.id.tvSearch);
+        String location = etSearch.getText().toString();
+        List<Address> addressList = null;
+        if(location != null || !location.equals("")){
+            Geocoder geocoder = new Geocoder(this);
+            try{
+                mMap.clear();
+                addressList = geocoder.getFromLocationName(location,1);
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title(address.getFeatureName()));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                Toast.makeText(getBaseContext(),address.getFeatureName(),Toast.LENGTH_LONG).show();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/heraya/mapsmarker/MapFragment.java
@@ -0,0 +1,8 @@
+package com.heraya.mapsmarker;
+
+import android.support.v4.app.Fragment;
+
+public class MapFragment extends Fragment {
+    public MapFragment(){}
+
+}
